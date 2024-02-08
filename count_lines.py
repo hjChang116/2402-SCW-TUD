392364ba4b780d3dfc6f988500baeec2569c82bb
hjChang116 <hjchang116@gmail.com>
2024-02-08 16:42:45 +0100

    count_lines.py change name style as initial and surname
    Done in GitHub

--- count_lines.py
+++ count_lines.py
@@ -2,7 +2,7 @@
 Aim: this script counts the number of lines in standard input
 Input: strings from the command line
 Output: change something
-Author: Jess Chang
+Author: J. Chang
 """
 import sys
 
